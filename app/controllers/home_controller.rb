be124f6d00a955e99650e7131720d6abaab9c0e5
Julian Dobmann <julian.dobmann@gmail.com>
2015-05-24 14:18:10 +0200

    Moved model logic from controller to model.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,22 +2,6 @@ class HomeController < ApplicationController
   require 'open-uri'
 
   def index
-    @interpret = get_rnd_title
-    @album = get_rnd_title 
-    @single = get_rnd_title
-    @cover_url = get_rnd_image_url
-  end
-
-  private
-  def get_rnd_title
-    url = "http://de.wikipedia.org/wiki/Spezial:Zuf%C3%A4llige_Seite"
-    doc = Nokogiri::HTML(open(url))
-    doc.css('#firstHeading').first.children
-  end
-
-  def get_rnd_image_url
-    url = "https://commons.wikimedia.org/wiki/Special:Random/File"
-    doc = Nokogiri::HTML(open(url))
-    doc.css('#file img').first["src"]
+    @record = TopRecord.create
   end
 end
